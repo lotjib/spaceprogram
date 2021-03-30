fd0ad0501440ff33c421b526e6ab185197b5f2f7
lotjib <parasolka1990@gmail.com>
2021-03-31 05:59:27 +0700

    up
    up

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,6 +9,22 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data(){
+  	return{
+  		stats: null,
+  	}
+  },
+  mounted(){
+  	this.getStats();
+  	console.log();
+  },
+  methods:{
+  	getStats(){
+  		axios.get('http://server.siberiaserver.tk/api/model/Stat/getAll').then(res => {
+  			this.stats = res.data;
+  		});
+  	}
+  },
 }
 </script>
