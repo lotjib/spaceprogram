f48e0fc29dbda5508b40730f5ea9ccd91e74f44a
lotjib <parasolka1990@gmail.com>
2021-03-31 04:02:04 +0700

    up
    up

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+  // настройки...
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/spaceprogram/'
+    : '/spaceprogram/'
+}
