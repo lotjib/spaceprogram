360f74d65e628c86a09f9847a6f6f0aeb7474cfd
lotjib <parasolka1990@gmail.com>
2021-03-31 04:12:04 +0700

    up
    up

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,12 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  // настройки...
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/spaceprogram/'
-    : '/spaceprogram/'
+ // настройки...
+ publicPath: process.env.NODE_ENV === 'production'
+   ? '/spaceprogram/'
+   : '/spaceprogram/',
+
+ transpileDependencies: [
+   'vuetify'
+ ]
 }
